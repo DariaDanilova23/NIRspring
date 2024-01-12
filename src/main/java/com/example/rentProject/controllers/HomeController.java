059ea2e16c24efb02bbcdab77e5400f9d0736851
DariaDanilova23 <danilowa.daria2010@mail.ru>
2024-01-12 15:41:44 +0500

    Проверка отображения инфы из бд

--- src/main/java/com/example/rentProject/controllers/HomeController.java
+++ src/main/java/com/example/rentProject/controllers/HomeController.java
@@ -1,14 +1,25 @@
 package com.example.rentProject.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 
+import com.example.rentProject.models.TypeOfRenter;
+import com.example.rentProject.services.TypeOfRenterServices;
+
 
 @Controller
 public class HomeController {
+  private final TypeOfRenterServices typeOfRenterServices;
+
+  public HomeController(TypeOfRenterServices typeOfRenterServices) {
+    this.typeOfRenterServices = typeOfRenterServices;
+}
+
    @GetMapping("/")
      public String getIndex(Model model){
         try{
@@ -18,5 +29,13 @@ public class HomeController {
         }
         return "index";
      }
+
+    @GetMapping("/renterType")
+    public String yourPage(Model model) {
+        List<TypeOfRenter> yourEntities = typeOfRenterServices.getAllTypesOfRenters();
+        model.addAttribute("yourEntities", yourEntities);
+
+        return "renterType";
+    }
     
 }
